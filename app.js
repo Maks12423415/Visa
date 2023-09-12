da35e8c2a4af04090662b06196e131d0d294031d
Maks Samborski <makxc7@gmail.com>
2023-09-12 20:14:07 +0200

    .

---
+++ app.js
@@ -0,0 +1,19 @@
+function numbers() {
+  var a = document.getElementById("inumbers").value;
+  var b = document.getElementById("h1");
+  b.innerHTML = a;
+}
+
+function names() {
+  var c = document.getElementById("names").value;
+  var d = document.getElementById("h2");
+
+  d.innerHTML = c;
+}
+
+function date() {
+  var e = document.getElementById("data").value;
+  var f = document.getElementById("h3");
+
+  f.innerHTML = e;
+}
